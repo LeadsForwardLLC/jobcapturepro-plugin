cccdad3e9605f2776c07949e6ea336840e66a37b
Integrations <integrations@leadsforward.com>
2025-02-04 09:24:30 -0500

    Adding GPLv2 license

--- jobcapturepro.php
+++ jobcapturepro.php
@@ -5,6 +5,8 @@
      * Version:           1.0.0
      * Author:            JobCapturePro
      * Author URI:        https://www.jobcapturepro.com/
+     * License: GPLv2 or later
+     * License URI: http://www.gnu.org/licenses/gpl-2.0.html
      */
 
 	// If this file is called directly, abort.
