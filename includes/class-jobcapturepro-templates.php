4d0dd8565cef5c3a7575079a6c147537f2901783
Ali Sbane <ali.sbane.dev@gmail.com>
2025-09-16 19:07:49 +0100

    Matched the design layout, plus used the available properties on the markup as needed

--- includes/class-jobcapturepro-templates.php
+++ includes/class-jobcapturepro-templates.php
@@ -1869,21 +1869,21 @@ public static function render_single_checkin($checkin, $company_info = array())
             <h2 class="jcp-company-name">' . esc_html($company_info['name']) . '</h2>';
         
         // Intro text
-        $output .= '<div class="jcp-company-into-text">
-            <p>Sarasota’s #1 for exterior cleaning. Trusted by 200+ homeowners.</p>
+        $output .= '<div class="jcp-company-info-text">
+            <p>' . esc_html($company_info['address']) . '</p>
         </div>';
 
         $output .= '<div class="jcp-company-div-2">';
 
         // Company Reviews text
-        // $output .= '<div class="jcp-company-reviews-text">
-        //     <p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z"/></svg> 4.9 (212 reviews)</p>
-        // </div>';
-
-        // Address
-       // $output .= '<div class="jcp-company-address">
-         //   <p>' . nl2br(esc_html($company_info['address'])) . '</p>
-        //</div>';
+        $output .= '<div class="jcp-company-reviews-text">
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                    <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z"/>
+                </svg>
+                <span>No Reviews</span>
+            </p>
+        </div>';
 
         
         // Check if we have either phone or URL
@@ -1891,7 +1891,7 @@ public static function render_single_checkin($checkin, $company_info = array())
         $has_url = !empty($company_info['url']);
         
         if ($has_phone) {
-            $output .= '<p> <strong> &nbsp;.&nbsp; </strong><a href="tel:' . esc_attr(preg_replace('/[^0-9]/', '', $company_info['tn'])) . '">' . esc_html($company_info['tn']) . '</a></p>';
+            $output .= '<p> <strong> &nbsp;.&nbsp; </strong><a href="tel:' . esc_attr(preg_replace('/[^0-9]/', '', $company_info['tn'])) . '">' . esc_html($company_info['phoneNumberString']) . '</a></p>';
         }
         
         if ($has_url) {
@@ -1909,13 +1909,18 @@ public static function render_single_checkin($checkin, $company_info = array())
             $output .= '<p class="jcp-no-contact-info">Contact No. and Website information not available</p>';
         }
         
-        $output .= '</div></div>'; // Close jcp-company-contact and jcp-company-details
+        $output .= '</div>'; // Close jcp-company-div-2
+
+        $output .= '<div class="jcp-company-description">
+            <p>' . esc_html($company_info['description']) . '</p>
+        </div>';
+        $output .= '</div>'; // jcp-company-details
 
          // Quote btn and text
         
             $output .= '<div class="jcp-company-logo">
-                <a href="#" class="quote-btn">Get a Quote</a>
-                <p class="powered-by">Powered by <b>JobCapturePro</b></p>
+            <a href="'. esc_url($company_info['quoteUrl']) .'" class="quote-btn">Get a Quote</a>
+            <p class="powered-by">Powered by <a href="https://jobcapturepro.com">JobCapturePro</a></p>
             </div>';
         
         
