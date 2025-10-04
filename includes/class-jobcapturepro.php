7b8bc1f543af469c6d00f362eac30fe04fbe183b
Ali Sbane <ali.sbane.dev@gmail.com>
2025-10-04 22:45:40 +0100

    Just formatting

--- includes/class-jobcapturepro.php
+++ includes/class-jobcapturepro.php
@@ -4,7 +4,8 @@
  * This file defines the core plugin class
  */
 
-class JobCaptureProPlugin {
+class JobCaptureProPlugin
+{
 
 	/**
 	 * The loader that's responsible for maintaining and registering all hooks that power the plugin.
@@ -30,8 +31,9 @@ class JobCaptureProPlugin {
 	/**
 	 * Define the core functionality of the plugin.
 	 */
-	public function __construct() {
-		if ( defined( 'JOBCAPTUREPROPLUGIN_VERSION' ) ) {
+	public function __construct()
+	{
+		if (defined('JOBCAPTUREPROPLUGIN_VERSION')) {
 			$this->version = JOBCAPTUREPROPLUGIN_VERSION;
 		} else {
 			$this->version = '1.0.0';
@@ -49,78 +51,84 @@ class JobCaptureProPlugin {
 	/**
 	 * Load the required dependencies for this plugin.
 	 */
-	private function load_dependencies() {
+	private function load_dependencies()
+	{
 
 		/**
 		 * The class responsible for orchestrating the actions and filters of the core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-jobcapturepro-loader.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-jobcapturepro-loader.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-jobcapturepro-admin.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-jobcapturepro-admin.php';
 
 		/**
 		 * The class responsible for generating HTML templates
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-jobcapturepro-templates.php';
-		
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-jobcapturepro-templates.php';
+
 		/**
 		 * The class responsible for defining all actions for public-facing shortcodes
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-jobcapturepro-shortcodes.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-jobcapturepro-shortcodes.php';
 
 		/**
 		 * The class responsible for defining the REST API
 		 */
-    	require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-jobcapturepro-api.php';
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-jobcapturepro-api.php';
 
+		/** 
+		 * The class responsible for rendering templates
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-template-loader.php';
 
 		// Create an instance of the loader which will be used to register the hooks with WordPress.
 		$this->loader = new JobCaptureProLoader();
-
 	}
 
 	/**
 	 * Register all of the hooks related to the admin area functionality of the plugin.
 	 */
-	private function define_admin_hooks(): void {
+	private function define_admin_hooks(): void
+	{
 
 		// Create an instance of the admin class
-		$plugin_admin = new JobCaptureProAdmin( plugin_name: $this->get_plugin_name(), version: $this->get_version() );
+		$plugin_admin = new JobCaptureProAdmin(plugin_name: $this->get_plugin_name(), version: $this->get_version());
 
 		// Register the admin_init hook with the corresponding function in the admin class
-		$this->loader->add_action( hook: 'admin_init', component: $plugin_admin, callback: 'jobcapturepro_settings_init' );
-
+		$this->loader->add_action(hook: 'admin_init', component: $plugin_admin, callback: 'jobcapturepro_settings_init');
 	}
 
 	/**
 	 * Register all of the shortcodes related to the public-facing functionality of the plugin.
 	 */
-    private function define_shortcodes(): void {
+	private function define_shortcodes(): void
+	{
 		// Create an instance of the shortcodes class
-        $plugin_shortcodes = new JobCaptureProShortcodes( $this->get_plugin_name(), $this->get_version(), $this->get_jobcapturepro_api_base_url() );
+		$plugin_shortcodes = new JobCaptureProShortcodes($this->get_plugin_name(), $this->get_version(), $this->get_jobcapturepro_api_base_url());
 
 		// Register the new shortcodes with proper plugin prefix
-		$this->loader->add_shortcode( shortcode: 'jobcapturepro_checkin', component: $plugin_shortcodes, callback: 'get_checkin' );
-		$this->loader->add_shortcode( shortcode: 'jobcapturepro_checkins', component: $plugin_shortcodes, callback: 'get_all_checkins' );
-		$this->loader->add_shortcode( shortcode: 'jobcapturepro_map', component: $plugin_shortcodes, callback: 'get_map' );
-        $this->loader->add_shortcode( shortcode: 'jobcapturepro_company_info', component: $plugin_shortcodes, callback: 'get_company_info' );
-		$this->loader->add_shortcode( shortcode: 'jobcapturepro_combined', component: $plugin_shortcodes, callback: 'get_combined_components' );
+		$this->loader->add_shortcode(shortcode: 'jobcapturepro_checkin', component: $plugin_shortcodes, callback: 'get_checkin');
+		$this->loader->add_shortcode(shortcode: 'jobcapturepro_checkins', component: $plugin_shortcodes, callback: 'get_all_checkins');
+		$this->loader->add_shortcode(shortcode: 'jobcapturepro_map', component: $plugin_shortcodes, callback: 'get_map');
+		$this->loader->add_shortcode(shortcode: 'jobcapturepro_company_info', component: $plugin_shortcodes, callback: 'get_company_info');
+		$this->loader->add_shortcode(shortcode: 'jobcapturepro_combined', component: $plugin_shortcodes, callback: 'get_combined_components');
 
 		// Keep old shortcodes for backwards compatibility
-		$this->loader->add_shortcode( shortcode: 'jcp_checkin', component: $plugin_shortcodes, callback: 'get_checkin' );
-		$this->loader->add_shortcode( shortcode: 'jcp_all_checkins', component: $plugin_shortcodes, callback: 'get_all_checkins' );
-		$this->loader->add_shortcode( shortcode: 'jcp_multimap', component: $plugin_shortcodes, callback: 'get_map' );
-        $this->loader->add_shortcode( shortcode: 'jcp_company_info', component: $plugin_shortcodes, callback: 'get_company_info' );
-		$this->loader->add_shortcode( shortcode: 'jcp_combined_components', component: $plugin_shortcodes, callback: 'get_combined_components' );
-    }
+		$this->loader->add_shortcode(shortcode: 'jcp_checkin', component: $plugin_shortcodes, callback: 'get_checkin');
+		$this->loader->add_shortcode(shortcode: 'jcp_all_checkins', component: $plugin_shortcodes, callback: 'get_all_checkins');
+		$this->loader->add_shortcode(shortcode: 'jcp_multimap', component: $plugin_shortcodes, callback: 'get_map');
+		$this->loader->add_shortcode(shortcode: 'jcp_company_info', component: $plugin_shortcodes, callback: 'get_company_info');
+		$this->loader->add_shortcode(shortcode: 'jcp_combined_components', component: $plugin_shortcodes, callback: 'get_combined_components');
+	}
 
 	/**
 	 * Register all API endpoints
 	 */
-	private function define_api_hooks(): void {
+	private function define_api_hooks(): void
+	{
 		new JobCaptureProAPI($this->get_jobcapturepro_api_base_url());
 	}
 
@@ -128,7 +136,8 @@ class JobCaptureProPlugin {
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
 	 */
-	public function run() {
+	public function run()
+	{
 		$this->loader->run();
 	}
 
@@ -136,29 +145,32 @@ class JobCaptureProPlugin {
 	 * The name of the plugin used to uniquely identify it within the context of
 	 * WordPress and to define internationalization functionality.
 	 */
-	public function get_plugin_name() {
+	public function get_plugin_name()
+	{
 		return $this->plugin_name;
 	}
 
 	/**
 	 * The reference to the class that orchestrates the hooks with the plugin.
 	 */
-	public function get_loader() {
+	public function get_loader()
+	{
 		return $this->loader;
 	}
 
 	/**
 	 * Retrieve the version number of the plugin.
 	 */
-	public function get_version() {
+	public function get_version()
+	{
 		return $this->version;
 	}
 
 	/**
 	 * Retrieve the base URL for the backend API.
 	 */
-	public function get_jobcapturepro_api_base_url() {
+	public function get_jobcapturepro_api_base_url()
+	{
 		return $this->jcp_api_base_url;
 	}
-
 }
