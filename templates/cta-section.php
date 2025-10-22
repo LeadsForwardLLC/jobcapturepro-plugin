cf6c1658a7e82063cd038defb520b3a3e0173745
Ali Sbane <ali.sbane.dev@gmail.com>
2025-10-22 14:31:47 +0100

    - fix: add margin to make space between shortcodes templates

--- templates/cta-section.php
+++ templates/cta-section.php
@@ -1,4 +1,4 @@
-<div class="jobcapturepro-cta-container">
+<div class="jobcapturepro-cta-container mt-8">
     <div class="jobcapturepro-cta">
         <h2>Let Your Work Speak For Itself</h2>
         <p>Capture check-ins like these with JobCapturePro. Set it and forget it.</p>
